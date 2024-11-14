413b18223256a14f88a63ae7d97ea9f0ec7e2a8a
saddadnabbil <saddadnabbil@gmail.com>
2024-11-14 10:15:09 +0700

    Initial Commit

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Saddad Nabbil on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
